e1f38850c0e15c2a54ffc5f7bf077149d85ac0c9
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-01-12 11:53:29 +0000

    wikipedia-viewer: add functioning input+button

--- pages/projects/wikipedia-viewer.js
+++ pages/projects/wikipedia-viewer.js
@@ -1,12 +1,32 @@
-import Link from "next/link";
+import { useState } from "react";
+import styles from "../../styles/wikipedia-viewer.module.scss";
 
 export default function WikipediaViewer() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  function getSearchQuery(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  function searchWikipedia(event) {
+    event.preventDefault();
+    getSearchQuery(event);
+    console.log("search", searchQuery);
+  }
+
   return (
-    <div>
-      <h1>WikipediaViewer</h1>
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
+    <div className={styles.WikipediaViewer}>
+      <h1>Wikipedia Viewer</h1>
+      <p>{searchQuery}</p>
+      <div className={styles.searchContainer}>
+        <input
+          type="search"
+          value={searchQuery}
+          onInput={getSearchQuery}
+          aria-label="Search wikipedia"
+        />
+        <button onClick={searchWikipedia}>Search</button>
+      </div>
     </div>
   );
 }
