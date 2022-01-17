274bf029a0510c5a4962499e787ef904883069b2
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-01-17 10:15:54 +0000

    twitch: set up project

--- pages/projects/twitch-streamers.js
+++ pages/projects/twitch-streamers.js
@@ -1,12 +1,28 @@
-import Link from "next/link";
+import { useState } from "react";
+import styles from "../../styles/twitch-streamers.module.scss";
 
 export default function TwitchStreamers() {
+  const [streamerData, setStreamerData] = useState([
+    { name: "ESL_SC2", isOnline: false, details: "" },
+    { name: "OgamingSC2", isOnline: false, details: "" },
+    { name: "cretetion", isOnline: false, details: "" },
+    { name: "freecodecamp", isOnline: false, details: "" },
+    { name: "storbeck", isOnline: false, details: "" },
+    { name: "habathcx", isOnline: false, details: "" },
+    { name: "RobotCaleb", isOnline: false, details: "" },
+    { name: "noobs2ninjas", isOnline: false, details: "" },
+  ]);
   return (
     <div>
-      <h1>TwitchStreamers</h1>
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
+      <h1>Twitch Streamers</h1>
+      <button>All</button>
+      <button>Online</button>
+      <button>Offline</button>
+      <ul>
+        {streamerData.map((streamer) => (
+          <li>{streamer.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
