1854a6bb10588482dc4d83124b8c812f998fe1ec
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-01-26 11:40:53 +0000

    Image-search: add functioning api call

--- pages/projects/image-search.js
+++ pages/projects/image-search.js
@@ -1,6 +1,21 @@
+import { createApi } from "unsplash-js";
 import styles from "../../styles/image-search.module.scss";
 
+const unsplash = createApi({
+  accessKey: process.env.NEXT_PUBLIC_API_KEY,
+});
+//
 export default function ImageSearch() {
+  unsplash.photos.get({ photoId: "N0e6W2WDa5U" }).then((result) => {
+    if (result.errors) {
+      // handle error here
+      console.log("error occurred: ", result.errors[0]);
+    } else {
+      // handle success here
+      const photo = result.response;
+      console.log(photo);
+    }
+  });
   return (
     <div>
       <h1>Image Search</h1>
