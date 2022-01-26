ebcf8dfa2b2348dcca375e405a6104fd6a318be1
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-01-26 13:06:29 +0000

    Image Search: add functional styling

--- pages/projects/image-search.js
+++ pages/projects/image-search.js
@@ -4,7 +4,9 @@ import styles from "../../styles/image-search.module.scss";
 const unsplash = createApi({
   accessKey: process.env.NEXT_PUBLIC_API_KEY,
 });
-//
+
+const topics = ["cats", "dogs", "mice"];
+
 export default function ImageSearch() {
   unsplash.photos.get({ photoId: "N0e6W2WDa5U" }).then((result) => {
     if (result.errors) {
@@ -16,9 +18,20 @@ export default function ImageSearch() {
       console.log(photo);
     }
   });
+
   return (
-    <div>
+    <div className={styles.ImageSearch}>
       <h1>Image Search</h1>
+      <form>
+        <input type="text" placeholder="Search.." name="search" />
+        <button type="submit">Search</button>
+      </form>
+      <div className={styles.buttonContainer}>
+        {topics.map((topic) => (
+          <button>{topic}</button>
+        ))}
+      </div>
+      <div></div>
     </div>
   );
 }
