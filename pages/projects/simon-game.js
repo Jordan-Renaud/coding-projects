361cba09e8f7259df3b0d11f3f354c9d9d42c5f8
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-01-28 13:13:09 +0000

    Simon Game: add buttons for game

--- pages/projects/simon-game.js
+++ pages/projects/simon-game.js
@@ -1,9 +1,55 @@
+import { useState } from "react";
 import styles from "../../styles/simon-game.module.scss";
 
 export default function SimonGame() {
+  const colours = [
+    { normal: "#ED1216", highlighted: "#F04245" },
+    { normal: "#DB7224", highlighted: "#E28F50" },
+    { normal: "#E7BA18", highlighted: "#ECC846" },
+    { normal: "#3EC13F", highlighted: "#65CD65" },
+    { normal: "#169DE9", highlighted: "#44B1EE" },
+    { normal: "#EC0CF3", highlighted: "#F03DF5" },
+    { normal: "#9023DC", highlighted: "#A64FE3" },
+    { normal: "#B89147", highlighted: "#C6A76C" },
+    { normal: "#49322C", highlighted: "#69483F" },
+    { normal: "#7C7D83", highlighted: "#96979C" },
+  ];
+
+  const [squares, setSquares] = useState([...Array(4).keys()]);
+  const [highlightedSquare, setHighlightedSquare] = useState(null);
+
+  function handleClick(event) {
+    const square = event.target.value;
+    setHighlightedSquare(Number(square));
+  }
+
   return (
     <div className={styles.SimonGame}>
       <h1>Simon Game</h1>
+      <form>
+        <label htmlFor="square-amount">Choose how many squares:</label>
+        <select name="square-amount" id="square-amount">
+          <option value="4">4</option>
+          <option value="6">6</option>
+          <option value="8">8</option>
+          <option value="10">10</option>
+        </select>
+      </form>
+      <div className={styles.gameBoard}>
+        {squares.map((square) => (
+          <button
+            key={square}
+            className={`${styles.square} ${
+              highlightedSquare === square
+                ? styles.highlighted
+                : "notHighlighted"
+            }`}
+            onClick={handleClick}
+            value={square}
+            style={{ backgroundColor: colours[square].normal }}
+          ></button>
+        ))}
+      </div>
     </div>
   );
 }
