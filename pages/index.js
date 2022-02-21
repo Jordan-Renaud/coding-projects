01e85946a549a5b1e46cb22ffeee3ce8c46c3707
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-02-21 11:49:39 +0000

    Home Page: remove google font

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,6 @@ export default function Home() {
       <Head>
         <title>Coding Projects</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <main>
         <Title texts={["FUN", "TAKE HOME", "LEARNING"]} />
