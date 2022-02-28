a294b9d08d13763ae3955bc5c545fdbf92153219
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-02-28 11:08:58 +0000

    Tic Tac Toe: add styling to setup options

--- components/projects/tic-tac-toe/SetupOptions.js
+++ components/projects/tic-tac-toe/SetupOptions.js
@@ -1,24 +1,30 @@
+import styles from "../../../styles/projects/tic-tac-toe.module.scss";
+
 export default function SetupOptions({ setNumberOfPlayers, gameMode }) {
   return (
-    <div>
-      <input
-        id="one-player"
-        type="radio"
-        name="one-player"
-        onChange={() => setNumberOfPlayers(1)}
-        value={1}
-        checked={gameMode === "onePlayer"}
-      />
-      <label htmlFor="one-player">One Player</label>
-      <input
-        id="two-player"
-        type="radio"
-        onChange={() => setNumberOfPlayers(2)}
-        name="two-player"
-        value={2}
-        checked={gameMode === "twoPlayer"}
-      />
-      <label htmlFor="two-player">Two Player</label>
+    <div className={styles.SetupOptions}>
+      <div className={styles.playerType}>
+        <input
+          id="one-player"
+          type="radio"
+          name="one-player"
+          onChange={() => setNumberOfPlayers(1)}
+          value={1}
+          checked={gameMode === "onePlayer"}
+        />
+        <label htmlFor="one-player">One Player</label>
+      </div>
+      <div className={styles.playerType}>
+        <input
+          id="two-player"
+          type="radio"
+          onChange={() => setNumberOfPlayers(2)}
+          name="two-player"
+          value={2}
+          checked={gameMode === "twoPlayer"}
+        />
+        <label htmlFor="two-player">Two Player</label>
+      </div>
     </div>
   );
 }
