a90833e5a68402321cab92e53bf55f3d64746e31
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-03-01 11:43:48 +0000

    Home Page: add styling for table

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ function MyApp({ Component, pageProps }) {
           href="https://jordan-renaud.netlify.app/"
           target="_blank"
           className="link"
+          rel="noreferrer"
         >
           Jordan Renaud
         </a>
