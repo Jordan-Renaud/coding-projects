d7a6fc6f4ec61b58066a327d6bbec8f71e548198
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-03-03 10:16:15 +0000

    Home Page: fix title not typing

--- components/home-page/Title.js
+++ components/home-page/Title.js
@@ -1,23 +1,26 @@
 import Typist from "react-typist";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../../styles/home-page/Title.module.scss";
 
 export default function Title({ texts }) {
   const [currentTextCounter, setCurrentTextCounter] = useState(0);
 
-  const waitingTime = (word) => (word.length > 4 ? 400 * word.length : 2000);
-  const timer = setTimeout(
-    () =>
-      currentTextCounter < texts.length - 1
-        ? setCurrentTextCounter(currentTextCounter + 1)
-        : setCurrentTextCounter(0),
-    waitingTime(texts[currentTextCounter])
-  );
+  function calculateTime() {
+    const waitingTime = (word) => (word.length > 4 ? 400 * word.length : 2000);
+
+    setTimeout(
+      () =>
+        currentTextCounter < texts.length - 1
+          ? setCurrentTextCounter(currentTextCounter + 1)
+          : setCurrentTextCounter(0),
+      waitingTime(texts[currentTextCounter])
+    );
+  }
 
   return (
     <h1 className={styles.Title}>
       <Typist
-        onTypingDone={() => clearTimeout(timer)}
+        onTypingDone={calculateTime}
         startDelay={500}
         avgTypingDelay={150}
         key={currentTextCounter}
